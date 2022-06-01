c8e49ddbdc102f51b0a8d5ffee5780990b859b7c
jrl103 <jrl103@naver.com>
2022-06-02 04:05:22 +0900

    파이어스토어 부분 주석 첨가

--- src/Detail.js
+++ src/Detail.js
@@ -1,42 +1,59 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
-import {useSelector, useDispatch} from "react-redux";
-import { deleteBucket, updateBucket, deleteBucketFB,updateBucketFB } from "./redux/modules/bucket";
-import Button from '@mui/material/Button';
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  deleteBucket,
+  updateBucket,
+  deleteBucketFB,
+  updateBucketFB,
+} from "./redux/modules/bucket";
+import Button from "@mui/material/Button";
 
 const Detail = (props) => {
-const dispatch = useDispatch();
-const history = useHistory();
-const params = useParams();
-const bucket_index = params.index;
-const bucket_list = useSelector((state) => state.bucket.list);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const params = useParams();
+  const bucket_index = params.index;
+  const bucket_list = useSelector((state) => state.bucket.list);
 
-return (
-  <div>
-    <h1>{bucket_list[bucket_index]? {bucket_list[bucket_index].text : ""}</h1>
-    <Button
-    variant="outlined"
-      onClick={() => {
-        // dispatch(updateBucket(bucket_index));
-        dispatch(updateBucketFB(bucket_list[bucket_index].id));
-      }}
-    >
-      완료하기
-    </Button>
-    <Button
-    variant="outlined" color="secondary"
-      onClick={() => {
-        // console.log("삭제하기 버튼을 눌렀어!");
-        // dispatch(deleteBucket(bucket_index));
-        dispatch(deleteBucketBK(bucket_list[bucket_index].id));
-        history.goBack();
-      }}
-    >
-      삭제하기
-    </Button>
-  </div>
-);
-}
+  return (
+    <div>
+      <h1>{bucket_list[bucket_index] ? bucket_list[bucket_index].text : ""}</h1>
+      // bucket_list의 bucket_index 번째 거가 없으므로 text라는 값을 가져올 수 없다는 에러가 뜸
+      // 이유 : FireStore에서 데이터를 가져오는데 시간이 필요함
+      // 그런데 bucket_list[bucket_index] 가 이미 화면에 그려지고 있는 거임
+      // so, 지금 bucket_list 데이터가 없는데, 없는 거에서 텍스트를 찾으려고 하니까 에러가 남
+      // --> 삼항연산자 사용!
+
+      
+      <Button
+        variant="outlined"
+        onClick={() => {
+          // dispatch(updateBucket(bucket_index));
+          dispatch(updateBucketFB(bucket_list[bucket_index].id));
+          // 우리한테 필요한 것은  bucket_index기 아니라 id !
+          // id는 FireStore에서 가지고 온 데이터에 들어있음 --> data.id 해주기 !
+          // (loadBucketFB, addBucketFB할 때 데이터 모양을 그렇게 만들어줌)
+          //
+        }}
+      >
+        완료하기
+      </Button>
+      <Button
+        variant="outlined"
+        color="secondary"
+        onClick={() => {
+          // console.log("삭제하기 버튼을 눌렀어!");
+          // dispatch(deleteBucket(bucket_index));
+          dispatch(deleteBucketFB(bucket_list[bucket_index].id));
+          
+          history.goBack();
+        }}
+      >
+        삭제하기
+      </Button>
+    </div>
+  );
+};
 
 export default Detail;
--- src/redux/modules/bucket.js
+++ src/redux/modules/bucket.js
@@ -10,6 +10,8 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 
+
+
 // Actions
 const LOAD = "bucket/LOAD";
 const CREATE = "bucket/CREATE";
@@ -21,6 +23,8 @@ const initialState = {
   is_loaded: false,
   list: [],
 };
+
+
 
 // Action Creators
 export function loadBucket(bucket_list) {
@@ -45,6 +49,11 @@ export function isLoaded(loaded) {
   return { type: LOADED, loaded };
 }
 
+
+
+
+
+
 // middlewares
 export const loadBucketFB = () => {
   return async function (dispatch) {
@@ -68,35 +77,66 @@ export const addBucketFB = (bucket) => {
   return async function (dispatch) {
     dispatch(isLoaded(false));
     const docRef = await addDoc(collection(db, "bucket"), bucket);
-    const bucket_data = { id: docRef.id, ...bucket };
+        // console.log((await getDoc(docRef)).data());
+    // docRef의 형식이 document 이므로 데이터를 가져오려면 'getDoc'을 써서 가져와야함.
+
+    const _bucket = await getDoc(docRef);
+    // 리덕스에 넣기 위해 필요한 정보 : 아이디, 도큐먼트 데이터들(텍스트, 컴플리트)
+    // await에서 document 하나에 있는 정보를 가지고 오는 것을 상수로 선언한다.
+    // getDoc으로 FireStore에 있는 정보를 가지고 왔다.
+    // 하지만 생각해보면 굳이 가져오지 않아도 bucket에 데이터가 전부 들어있다.
+    // 필요한 것 : id
+    // 그럼 굳이 FireStore에서 정보를 가져오기 위해 기다리지 않아도 된다.
+    
+
+    const bucket_data = { id: _bucket.id, ...bucket.data() };
+    // const bucket_data = { id: docRef.id, ...bucket}; 라고 해도 됨
+    // 이미 버킷에 다 들어있으므로
+    // id 는 docRef 에 있는 id이고, 이 버킷에 있는 내용 전부 꺼내 줘라! 리고 할 수도 있음.
 
     dispatch(createBucket(bucket_data));
+
+    // 혹은 bucket_data 선언하지 않고,
+    // dispatch(createBucket({ id: _bucket.id, ...bucket.data() })); 로 해도됨
   };
 };
 
 export const updateBucketFB = (bucket_id) => {
+  // 기능이 '완료하기' 뿐이기 때문에 bucket 데이터는 필요없음. id만 가져옴.
   return async function (dispatch, getState) {
-    // console.log(bucket_id)
+    //두번째 인자 getState로 bucket_id 받아와서 리덕스 수정할때 사용 !
+    
+    // console.log(bucket_id) 잘가져오는지 확인
 
     const docRef = doc(db, "bucket", bucket_id);
     // 어떤 것을 업데이트 해줄건지. doc(내가 설정한 파이어스토어, 어떤 컬렉션, 도큐먼트 아이디)
 
     await updateDoc(docRef, { completed: true });
     // updateDoc(업데이트 해줄 도큐먼트, {어떻게 수정할지})
+    // 끝나면 리덕스 데이터 수정해줘야 하기 때문에 앞에 await 걸기 !
 
-    console.log(getState().bucket); // 버킷의 리스트 정보 긁어오기
+    console.log(getState().bucket);
+    // 버킷의 리스트 정보 긁어오기(두번째 인자 getState 사용 !)
 
     const _bucket_list = getState().bucket.list;
+    // 여기서 이제 index를 찾아줘야함.
+    // 이유 : updateBucket 액션이 index를 받기 때문 !
+
     const bucket_index = _bucket_list.findIndex((b) => {
       return b.id === bucket_id;
+      // --> array의 내장함수 findIndex로 index 찾기 !
+      // b(_bucket_list)가 갖고 있는 id가, bucket_id 와 같으면 리턴해주기 !
+
       // 파라미터로 받아온 버킷아이디와 아이디가 똑같은 것이 있니?
       // 있다면 그것의 인덱스 가져와
     });
 
-    // console.log(bucket_index)
+    // console.log(bucket_index) 잘 나오는 지 확인
 
     dispatch(updateBucket(bucket_index));
-    // 가져온 인덱스 넣어서 업데이트
+    // updateBucket 액션 일으키기 ! 인자는 가져온 bucket_index !
+     
+
   };
 };
 
@@ -105,18 +145,34 @@ export const deleteBucketFB = (bucket_id) => {
     if (!bucket_id) {
       window.alert("아이디가 없네요!");
       return;
+      // 밑의 구문을 들어가지 못하게 하기
+      // 버킷 아이디가 없을 경우 에러가 뜨는 것을 방지하기 위해 만듦
+      // --> 파라미터가 꼭 필요한 것들을 작업할 때는, 파라미터가 없을 경우도 대비해야함
+      // --> 원래는 업데이트에도 들어가야함
     }
     const docRef = doc(db, "bucket", bucket_id);
+    // 뭐를 지울지 선언
+  
     await deleteDoc(docRef);
-
-    const _bucket_list = getState().bucket.list;
+    // 어떤 도큐먼트 지울지 쓰기 --> docRef
+  
+    // --리덕스에 들어가 있는 데이터 지워주기----
+    const _bucket_list = getState().bucket.list; // 버킷에 있는 리스트 정보 긁어오기
+    
     const bucket_index = _bucket_list.findIndex((b) => {
       return b.id === bucket_id;
+      // 이 리스트 정보에서 파라미터로 받아온 bucket_id랑 id가 똑같은 게 있니?
+      // id가 똑같은 게 있다면 걔 인덱스 가져와
     });
 
     dispatch(deleteBucket(bucket_index));
+    // 가져온 인덱스 넣어서 업데이트 해주기
   };
 };
+
+
+
+
 
 // Reducer
 export default function reducer(state = initialState, action = {}) {
